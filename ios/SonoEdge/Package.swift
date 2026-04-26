f27d3bfe30ba8d7636bd66690424d44f60f4406f
RaeXuPCinPrimebot <xrjgoole@gmail.com>
2026-04-27 00:02:04 +0800

    改用podfile

--- ios/SonoEdge/Package.swift
+++ ios/SonoEdge/Package.swift
@@ -4,16 +4,11 @@ import PackageDescription
 let package = Package(
     name: "SonoEdge",
     platforms: [.iOS(.v16)],
-    dependencies: [
-        .package(url: "https://github.com/tensorflow/tensorflow.git",
-                 from: "2.16.0"),
-    ],
+    dependencies: [],
     targets: [
         .executableTarget(
             name: "SonoEdge",
-            dependencies: [
-                .product(name: "TensorFlowLiteSwift", package: "tensorflow"),
-            ],
+            dependencies: [],
             path: "Sources",
             resources: [
                 .copy("Models/heart_quality_int8full.tflite"),
